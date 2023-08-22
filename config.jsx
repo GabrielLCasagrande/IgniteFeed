ae93ac6427bec3049bcb3d7d1a05424b7fec0c18
Gabriel <gabriellourencinicasagrande330@gmail.com>
2023-08-22 17:19:04 -0300

    ViteError

---
+++ config.jsx
@@ -0,0 +1,4 @@
+// para acessar o projeto no vite digitar cd vite-project no terminal e npm run dev
+
+// Propriedades e componentes
+// propriedades são informações que passamos para os componentes, componente por exemplo "post de facebook" ou seja, cada post é um componente, porém cada post é diferente um do outro como auto, likes e imagem.
--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,12 @@
-function App() {
-  return <h1>Hello World</h1>
+import { Post } from "./Post"
+
+export function App() {
+  return (
+    <div>
+      <Post
+        author="Gabriel Casagrande"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, esse veniam dolorem qui totam dolores voluptatem beatae aliquam deserunt alias eos soluta. Debitis expedita nesciunt asperiores. Dolorum provident exercitationem saepe?"
+      />
+    </div>
+  )
 }
-
-export default App
-
-//http://localhost:5173/
